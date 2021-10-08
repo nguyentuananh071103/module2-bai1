817a7d35bff21f835e30b5b656634782647d240e
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-10-08 17:25:00 +0700

    add a file

---
+++ ungDungProductDiscountCalculator.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <label>
+        Product Description
+        <input type="text" name="Product Description" size="30"  placeholder="Product Description" />
+    </label>
+    <br>
+    <label>
+        List Price
+        <input type="text" name="List Price" size="30"  placeholder="List Price" />
+    </label>
+    <br>
+    <label>
+        Discount Percent
+        <input type="text" name="Discount Percent" size="30"  placeholder="Product Description" />
+    </label>
+    <input type="submit" value="Calculate Discount "/>
+</form>
+
+</body>
+</html>
